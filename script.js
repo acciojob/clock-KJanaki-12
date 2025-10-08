74d84a90f94bdda5e0ac2665074935453e4c3014
KJanaki-12 <kenchugundujanaki@gmail.com>
2025-10-08 19:14:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let timerDisplay=document.getElementById("timer");
+
+let currentTime = new Date();
+
+setInterval(
+	function() {
+		currentTime = new Date();
+		timerDisplay.innerHTM=currentTime.toLocaleString();
+	},1000
+);
